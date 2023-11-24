6e0cf14a06828483b7e404ab00cfea243512d37e
Your NamePritom003 <njahanpritom65@gmail.com>
2023-11-24 19:47:26 +0600

    sign in

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import google from '../../assets/icons8-google.svg';
+import Navbar from '../Shared/Navbar/Navbar';
+import Footer from '../Shared/Footer';
 const Login = () => {
   return (
-    <div>
-      login
+  <div>
+    <Navbar></Navbar>
+      <div className="hero min-h-[70vh] bg-base-200">
+    <div className="hero-content flex-col lg:flex-row">
+       <div className='grid  align-middle items-center justify-center'>
+       <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold text-[#7C93C3] ">Login Now</h1>
+        </div>
+    <form  className="card-body">
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="password" name='password' 
+              placeholder="password" className="input  w-96 input-bordered" required />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+            <button className="btn bg-[#265073]
+             text-white font-bold text-xl">Login</button>
+          </div>
+            <p>
+              Have an account?{' '}
+              <Link className='underline-offset-8 
+               text-cyan-600 font-bold' to='/regi'>
+                SignUP
+              </Link>
+            </p>
+          </form>
+  
+  
+       </div>
+  
+  
+  
+  
+  
+  
+  
+       <div>
+    <button className='flex h-20 btn btn-outline text-white bg-[#265073] justify-center  items-center align-middle'> Login
+    <img className='h-[60px]' src={google} alt="" />Google </button>
+
     </div>
+    </div>
+  </div>
+  <Footer></Footer>
+  </div>
   );
 };
 
--- src/Pages/signup/SignUp.jsx
+++ src/Pages/signup/SignUp.jsx
@@ -1,10 +1,94 @@
-import React from 'react';
+
+import { Link } from 'react-router-dom';
+import google from '../../assets/icons8-google.svg';
 
 const SignUp = () => {
   return (
+
+<div className=''>
+<div className="hero   bg-base-200">
+  <div className="hero-content  flex-col lg:flex-row">
+     <div className='grid  align-middle items-center justify-center'>
+     <div className="text-center lg:text-left">
+        <h1 className="text-5xl font-bold text-[#7C93C3] ">Sign UP</h1>
+      </div>
+  <form  className="card-body max-h-[80vh] ">
+  <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input type="text" name='name' placeholder="your name" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo Url</span>
+            </label>
+            <input type="photo" name='image' placeholder="url" className="input input-bordered w-96" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+            <p>
+            Have an account?{' '}
+            <Link className='underline-offset-8 
+             text-cyan-600 font-bold' to='/login'>
+              Login
+            </Link>
+          </p>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn bg-[#265073] text-white font-bold text-xl">Sign up</button>
+          </div>
+        
+        </form>
+
+
+     </div>
+
+
+
+
+
+
+
     <div>
-      signup
+    <button className='flex h-20 btn btn-outline text-white bg-[#265073] justify-center  items-center align-middle'> signup with
+    <img className='h-[60px]' src={google} alt="" />Google </button>
+
     </div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   );
 };
 
